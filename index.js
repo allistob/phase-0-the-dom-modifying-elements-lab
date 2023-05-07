cdfec01981c8aea70924a53141290ebf844ad3d2
Allison Tobar <allisontobarrodriguez@gmail.com>
2023-05-06 23:02:37 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+const nodeToRemove = document.querySelector('main#main');
+nodeToRemove.remove();
+const newHeader = document.createElement('h1');
+newHeader.textContent = 'New Header Text';
+newHeader.classList.add('header-class');
+newHeader.setAttribute('id', 'victory');
+const yourName = 'Allison';
+newHeader.textContent = `${yourName} is the champion`;
+newHeader.classList.add('header-class');
+newHeader.setAttribute('id', 'victory');
+document.body.appendChild(newHeader);
